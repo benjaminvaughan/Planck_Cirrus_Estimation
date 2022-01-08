0b4c90cae7f9771d0211fab4d9828e4b97009347
benjaminvaughan <vaughanb18@gmail.com>
2022-01-07 21:15:33 -0500

    fixed a bug where if given a value at or near 0 degrees the map would not wrap around properly.

--- make_map.py
+++ make_map.py
@@ -26,6 +26,7 @@ from astropy.coordinates import SkyCoord
 from scipy.interpolate import griddata
 from astropy.wcs.utils import pixel_to_skycoord, skycoord_to_pixel
 import planck_config
+from reproject import reproject_interp, reproject_from_healpix
 
 def create_map(ref_head, nu):
     '''
@@ -47,7 +48,7 @@ def create_map(ref_head, nu):
 
     filenames = [tau_name, temp_name, beta_name]
 
-
+    #fix astrometry assuming square (astropy prefers CD matrices)
     if 'CD1_1' in ref_head.keys():
         ref_pixsize = 3600 *  np.mean([abs(ref_head['CD1_1'] + ref_head['CD2_1']), abs(ref_head['CD2_1'] + ref_head['CD2_2'])])
     elif 'CDELT1' in ref_head.keys():
@@ -56,50 +57,22 @@ def create_map(ref_head, nu):
         ref_head['CD2_1'] = 0
         ref_head['CD2_2'] = ref_head['CDELT2']
         ref_pixsize = 3600 *  np.mean([abs(ref_head['CD1_1'] + ref_head['CD2_1']), abs(ref_head['CD2_1'] + ref_head['CD2_2'])])
-    ref_mapsize = [ref_head['NAXIS2'], ref_head['NAXIS1']]
-    center = [ref_head['CRVAL1'], ref_head['CRVAL2']]
-
-    #this is to oversize the map so that the data has a larger area than the interpolating grid to reduce edge effects
-    ref_head['naxis1'] = ref_head['naxis1'] * 2
-    ref_head['naxis2'] = ref_head['naxis2'] * 2
-    ref_head['crpix1'] = ref_head['crpix1'] * 2
-    ref_head['crpix2'] = ref_head['crpix2'] * 2
-
 
     I_to_MJy = 1e20 #converts from standard units of Intensity to MJy
     param_values = [] #Beta = 2, Temperature = 1, Tau = 0
     for f in filenames:
-        data, pixsize, x_side, y_side, ra, dec = read_in_fits(f, center, ref_head, ref_pixsize, ref_mapsize)
+        data, ra_grid, dec_grid = read_in_fits(f, ref_head) #, pixsize, x_side, y_side, ra, dec
         param_values.append(data)
-    I = calc_intensity(param_values[2], param_values[0], param_values[1], nu)
-
-    I_map = np.reshape(I, (x_side, y_side)) * I_to_MJy
-
-
-    #this fixes the astrometry in the map to account for the oversizing done previously
-    ref_head['naxis1'] = ref_head['naxis1'] / 2
-    ref_head['naxis2'] = ref_head['naxis2'] / 2
-    ref_head['crpix1'] = ref_head['crpix1'] / 2
-    ref_head['crpix2'] = ref_head['crpix2'] / 2
-
-    interped_map, ragrd, decgrd = interp_back_to_ref(I_map, ra, dec, ref_head)
-
-
-    return interped_map, ragrd, decgrd
+    I_map = calc_intensity(param_values[2], param_values[0], param_values[1], nu) * I_to_MJy
+    return I_map, ra_grid, dec_grid
     # return interped_map
 
-def read_in_fits(filename, center, ref_head, ref_pixsize=8, ref_mapsize=260):
+def read_in_fits(filename, ref_head):
     '''
     Purpose : This function reads in the fits files for the components and parses them so that we are left with data only for our field.
     Inputs: filename (str) - the singular filename for a component used in the MBB fit
-            center (float array) - center of the field of interest
             ref_head (Astropy.header) - header for the reference field
-            ref_pixsize - pixel size of the reference map
-            ref_mapsize - mapsize of the reference map
-    Outputs: map (float array) - an array of flux values at the given field
-             pixsize (float) - pixel size of the uninterpolated component maps
-             x_side (int) - the length of the x-axis of the map
-             y_side (int) - the length of the y-axis of the map
+    Outputs: map (float array) - an array of data values at the given field
              RA_grid (float array) - grid of the Right Ascension values used to pull out components
              DEC_grid (float array) - grid of the Declination values used to pull out components
     '''
@@ -123,44 +96,26 @@ def read_in_fits(filename, center, ref_head, ref_pixsize=8, ref_mapsize=260):
     order = head['ORDERING']
     hdul.close()
 
-    #Galactic Coordinate System
-    hp = HEALPix(nside=nside, order=order, frame='galactic')
-    #create a pixel grid in terms of the nu=353 grid for GNILC to create our intensity maps
-    pixsize = hp.pixel_resolution.to(u.arcsecond).value
-
-    #* 10 is for boosting the size of the map so to fix edge effects from interpolation
-    map_arc_x = ref_mapsize[0] * 2 * ref_pixsize #map size in arcseconds
-    map_arc_y = ref_mapsize[1] * 2 * ref_pixsize
-
-    npixxside = ceil(map_arc_x / pixsize) #convert to map size in pixels for nu = 353 map.
-    npixyside = ceil(map_arc_y / pixsize)
-
-    #* 10 is for boosting the size of the map so to fix edge effects from interpolation
-    x  = np.linspace(0, ref_mapsize[0] * 2,   npixxside)
-    y  = np.linspace(0, ref_mapsize[1] * 2,   npixyside)
+    if order == 'RING':
+        nested = False
+    else:
+        nested = True
+    map, mask = reproject_from_healpix(filename, world(ref_head), shape_out=(ref_head['NAXIS1'], ref_head['NAXIS2']), nested=nested)
+    print(map.shape)
+    x  = np.arange(0, ref_head['NAXIS2'])
+    y  = np.arange(0, ref_head['NAXIS1'])
 
     X, Y = np.meshgrid(x, y)
     w = world(ref_head)
-    skycoords = pixel_to_skycoord(X.ravel(), Y.ravel(), wcs=w, origin=0)
+    skycoords = pixel_to_skycoord(X.ravel(), Y.ravel(), wcs=world(ref_head), origin=0)
     RA_grid = np.asarray(skycoords.ra.to_string(decimal=True), dtype='float') * u.deg
     DEC_grid = np.asarray(skycoords.dec.to_string(decimal=True), dtype='float') * u.deg
-
-
-
-
-    # coords = SkyCoord(RA_grid.ravel(), DEC_grid.ravel(), frame='icrs')
-    coords = SkyCoord(ra=RA_grid.ravel(), dec=DEC_grid.ravel(), frame='icrs')
-    gal_coords = coords.galactic
-
-    map = hp.interpolate_bilinear_skycoord(gal_coords, data)
-
-    x_side = len(x)
-    y_side = len(y)
-    return map, pixsize, y_side, x_side, RA_grid, DEC_grid
+    return map, RA_grid, DEC_grid
 
 
 def interp_back_to_ref(img, ra, dec, ref_head):
     '''
+    this function is depreciated use reproject_interp from astropy instead
     Purpose: Perform the inperolation of the completed Planck Map to a reference header
     Inputs: img - the Planck flux map
             ra  - grid of the Right Ascension values used to create the Planck Map
